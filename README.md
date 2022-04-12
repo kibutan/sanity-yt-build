3503437001e3167e832ebe83f5f7585c91925fc0
kibutan <s1170230@gmail.com>
2022-04-12 22:33:52 +0900

    1:06:39

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 https://www.youtube.com/watch?v=6fNy0iD3hsk&t=126s
 
 46:45
+concole1:yarn dev
+console2:
+cd sanityyoutube
+sanity start
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts)
+
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -34,4 +41,17 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+  _id,title,slug,author->{
+  name,image
+},description,
+mainImage,slug
+}`
+  const posts = await sanityClient.fetch(query)
+  return {
+    props: {
+      posts,
+    },
+  }
+}
